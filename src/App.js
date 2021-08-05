eefdc5c7ed6ee0a86e02ed44a7f632a63d2d3e1d
bec3x <bec3x@users.noreply.github.com>
2021-08-04 21:09:50 -0500

    changing to functional react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { useState } from 'react'
 import './index.css'
 
 const BUTTONS = [
@@ -17,96 +18,164 @@ const BUTTONS = [
   { id: 12, val: '3'},
   { id: 13, val: '/'},  
 ]
-// TODO: MAKE DISPLAY AND NUMBERS A LITERAL VALUE NOT LISTS AND THEN USE EXPRESSION STATE TO BUILD UP THE NUMBERS
-export class App extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      display: [],
-      number1: [],
-      operator: 0,
-      number2: [],
-    }
-  }
-
-  onClick = (e) => {
-    if (parseInt(e.target.id) !== 0) {
-      this.buildExpression(parseInt(e.target.id), e.target.value);
-    } else {
-      this.evaluate();
-    }
-  }
-
-  onClear = () => {
-    this.setState({ number1: [], operator: 0, number2: [], display:  [] });
-  }
-
-  // build up expression state. set display to the expression. when operator is given, set display to the corresponding number.
-  buildExpression(id, value) {
-    if (id !== 1 && id !== 5 && id !== 9 && id !== 13) {
-      this.setState({ display: [...this.state.display, value] });
-    } else {
-      !this.state.operator ? this.setState({ number1: this.state.display }) : this.setState({ number2: this.state.display });
-      this.setState({ operator: id, display: [] });
-    }
-  }
-
-  evaluate() {
-    const lhs = parseInt(this.state.number1.join(''));
-    const rhs = parseInt(this.state.number2.join(''));
+export default function App() {
+  const [display, setDisplay] = useState(0);
+  const [numbers, setNumbers] = useState([]);
+  const [operator, setOperator] = useState(0);
+  const [expression, setExpression] = useState([]);
+  
+  // Not using best practice -> don't call hooks in loops, conditions, or nested functions
+  const onClick = (e) => {
+    const id = parseInt(e.target.id);
+    const value = e.target.value;
     
-    let value;
-    switch(this.state.operator) {
-      case 1:
-        value = lhs + rhs;
-        break;
-      case 5:
-        value = lhs - rhs; // wrong when negative!
-        break;
-      case 9:
-        value = lhs * rhs;
-        break;
-      case 13:
-        value = lhs / rhs; // wrong!
-        break;
-      default:
-        value = lhs; // wrong!
-    }
-
-    this.setState({ display: this.splitValue(value) });
+    id !== 0 ? buildExpression(id, value) : evaluate();
   }
 
-  // I dont think that this is needed.
-  splitValue(value) {
-    let valList = [];
+  const onClear = () => {
+    setDisplay(0);
+    setNumbers([]);
+    setOperator(0);
+    setExpression([]);
+  }
 
-    valList.push((value % 10).toString());
-    value = Math.floor(value / 10);
+  const buildExpression = (id, value) => {
     
-    while (value > 0) {
-      valList = [(value % 10).toString(), ...valList];
-      value = Math.floor(value / 10);
+    if (parseInt(value)) {
+      const newExpression = [...expression, value];
+      setExpression(newExpression);
+      setDisplay(parseInt(newExpression.join('')));
+    } else {
+      setNumbers([...numbers, display]);
+      setExpression([]);
+      setDisplay(0);
+      setOperator(id);
     }
-
-    return valList;
   }
 
-  render() {
-    return (
+  const evaluate = () => {
+    const [number1, number2] = [...numbers, display];
+    setNumbers(numbers.push(number2));
+
+    const value = number1 / number2;
+    setDisplay(value);
+  }
+
+  return (
+    <div>
       <div className='calculator'>
         <div className='display'>
-          <p>{this.state.display}</p>
+          <p>{display}</p>
         </div>
         <div className='buttons'>
-          <input type='button' id='clear' value='clear' className='op' onClick={this.onClear}></input>
+          <input type='button' id='clear' value='clear' className='op' onClick={onClear} />
           {BUTTONS.map((button =>
-            <input key={button.id} type='button' id={button.id} value={button.val} className={parseInt(button.val) ? 'num' : 'op' } onClick={this.onClick}></input>
+            <input 
+              key={button.id} 
+              type='button' 
+              id={button.id} 
+              value={button.val} 
+              className={parseInt(button.val) ? 'num' : 'op' }
+              onClick={onClick}
+            />  
           ))}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-export default App
+// // TODO: MAKE DISPLAY AND NUMBERS A LITERAL VALUE NOT LISTS AND THEN USE EXPRESSION STATE TO BUILD UP THE NUMBERS
+// export class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       display: [],
+//       number1: [],
+//       operator: 0,
+//       number2: [],
+//     }
+//   }
+
+//   onClick = (e) => {
+//     if (parseInt(e.target.id) !== 0) {
+//       this.buildExpression(parseInt(e.target.id), e.target.value);
+//     } else {
+//       this.evaluate();
+//     }
+//   }
+
+//   onClear = () => {
+//     this.setState({ number1: [], operator: 0, number2: [], display:  [] });
+//   }
+
+//   // build up expression state. set display to the expression. when operator is given, set display to the corresponding number.
+//   buildExpression(id, value) {
+//     if (id !== 1 && id !== 5 && id !== 9 && id !== 13) {
+//       this.setState({ display: [...this.state.display, value] });
+//     } else {
+//       !this.state.operator ? this.setState({ number1: this.state.display }) : this.setState({ number2: this.state.display });
+//       this.setState({ operator: id, display: [] });
+//     }
+//   }
+
+//   evaluate() {
+//     const lhs = parseInt(this.state.number1.join(''));
+//     const rhs = parseInt(this.state.number2.join(''));
+    
+//     let value;
+//     switch(this.state.operator) {
+//       case 1:
+//         value = lhs + rhs;
+//         break;
+//       case 5:
+//         value = lhs - rhs; // wrong when negative!
+//         break;
+//       case 9:
+//         value = lhs * rhs;
+//         break;
+//       case 13:
+//         value = lhs / rhs; // wrong!
+//         break;
+//       default:
+//         value = lhs; // wrong!
+//     }
+
+//     this.setState({ display: this.splitValue(value) });
+//   }
+
+//   // I dont think that this is needed.
+//   splitValue(value) {
+//     let valList = [];
+
+//     valList.push((value % 10).toString());
+//     value = Math.floor(value / 10);
+    
+//     while (value > 0) {
+//       valList = [(value % 10).toString(), ...valList];
+//       value = Math.floor(value / 10);
+//     }
+
+//     return valList;
+//   }
+
+//   render() {
+//     return (
+//       <div className='calculator'>
+//         <div className='display'>
+//           <p>{this.state.display}</p>
+//         </div>
+//         <div className='buttons'>
+//           <input type='button' id='clear' value='clear' className='op' onClick={this.onClear}></input>
+//           {BUTTONS.map((button =>
+//             <input key={button.id} type='button' id={button.id} value={button.val} className={parseInt(button.val) ? 'num' : 'op' } onClick={this.onClick}></input>
+//           ))}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
